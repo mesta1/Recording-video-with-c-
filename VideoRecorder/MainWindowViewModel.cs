8502721f11c7eac4e8db809044013e89de860d4c
Michele Cattafesta <michele.cattafesta@mesta-automation.com>
2017-04-05 22:10:54 +0100

    1st commit.
    Signed-off-by: Michele Cattafesta <michele.cattafesta@mesta-automation.com>

---
+++ VideoRecorder/MainWindowViewModel.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Accord.Video.FFMPEG;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using Microsoft.Win32;
+
+namespace VideoRecorder
+{
+    internal class MainWindowViewModel : ObservableObject, IDisposable
+    {
+        #region Private fields
+
+        private FilterInfo _currentDevice;
+
+        private BitmapImage _image;
+        private string _ipCameraUrl;
+
+        private bool _isDesktopSource;
+        private bool _isIpCameraSource;
+        private bool _isWebcamSource;
+
+        private IVideoSource _videoSource;
+        private VideoFileWriter _writer;
+        private bool _recording;
+        private DateTime? _firstFrameTime;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowViewModel()
+        {
+            VideoDevices = new ObservableCollection<FilterInfo>();
+            GetVideoDevices();
+            IsDesktopSource = true;
+            StartSourceCommand = new RelayCommand(StartCamera);
+            StopSourceCommand = new RelayCommand(StopCamera);
+            StartRecordingCommand = new RelayCommand(StartRecording);
+            StopRecordingCommand = new RelayCommand(StopRecording);
+            SaveSnapshotCommand = new RelayCommand(SaveSnapshot);
+            IpCameraUrl = "http://88.53.197.250/axis-cgi/mjpg/video.cgi?resolution=320×240";
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<FilterInfo> VideoDevices { get; set; }
+
+        public BitmapImage Image
+        {
+            get { return _image; }
+            set { Set(ref _image, value); }
+        }
+
+        public bool IsDesktopSource
+        {
+            get { return _isDesktopSource; }
+            set { Set(ref _isDesktopSource, value); }
+        }
+
+        public bool IsWebcamSource
+        {
+            get { return _isWebcamSource; }
+            set { Set(ref _isWebcamSource, value); }
+        }
+
+        public bool IsIpCameraSource
+        {
+            get { return _isIpCameraSource; }
+            set { Set(ref _isIpCameraSource, value); }
+        }
+
+        public string IpCameraUrl
+        {
+            get { return _ipCameraUrl; }
+            set { Set(ref _ipCameraUrl, value); }
+        }
+
+        public FilterInfo CurrentDevice
+        {
+            get { return _currentDevice; }
+            set { Set(ref _currentDevice, value); }
+        }
+
+        public ICommand StartRecordingCommand { get; private set; }
+
+        public ICommand StopRecordingCommand { get; private set; }
+
+        public ICommand StartSourceCommand { get; private set; }
+
+        public ICommand StopSourceCommand { get; private set; }
+
+        public ICommand SaveSnapshotCommand { get; private set; }
+
+        #endregion
+
+        private void GetVideoDevices()
+        {
+            var devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in devices)
+            {
+                VideoDevices.Add(device);
+            }
+            if (VideoDevices.Any())
+            {
+                CurrentDevice = VideoDevices[0];
+            }
+            else
+            {
+                MessageBox.Show("No webcam found");
+            }
+        }
+
+        private void StartCamera()
+        {
+            if (IsDesktopSource)
+            {
+                var rectangle = new Rectangle();
+                foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+                {
+                    rectangle = Rectangle.Union(rectangle, screen.Bounds);
+                }
+                _videoSource = new ScreenCaptureStream(rectangle);
+                _videoSource.NewFrame += video_NewFrame;
+                _videoSource.Start();
+            }
+            else if (IsWebcamSource)
+            {
+                if (CurrentDevice != null)
+                {
+                    _videoSource = new VideoCaptureDevice(CurrentDevice.MonikerString);
+                    _videoSource.NewFrame += video_NewFrame;
+                    _videoSource.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Current device can't be null");
+                }
+            }
+            else if (IsIpCameraSource)
+            {
+                _videoSource = new MJPEGStream(IpCameraUrl);
+                _videoSource.NewFrame += video_NewFrame;
+                _videoSource.Start();
+            }
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                if (_recording)
+                {
+                    if (_firstFrameTime != null)
+                    {
+                        _writer.WriteVideoFrame(eventArgs.Frame, DateTime.Now - _firstFrameTime.Value);
+                    }
+                    else
+                    {
+                        _writer.WriteVideoFrame(eventArgs.Frame);
+                        _firstFrameTime = DateTime.Now;
+                    }
+                }
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    Image = bitmap.ToBitmapImage();
+                }
+                Image.Freeze(); // avoid cross thread operations and prevents leaks
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error on _videoSource_NewFrame:\n" + exc.Message, "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                StopCamera();
+            }
+        }
+
+        private void StopCamera()
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.Stop();
+                _videoSource.NewFrame -= video_NewFrame;
+            }
+            Image = null;
+        }
+
+
+        private void StopRecording()
+        {
+            _recording = false;
+            _writer.Close();
+            _writer.Dispose();
+        }
+
+        private void StartRecording()
+        {
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "Video1";
+            dialog.DefaultExt = ".avi";
+            dialog.AddExtension = true;
+            var dialogresult = dialog.ShowDialog();
+            if (dialogresult != true)
+            {
+                return;
+            }
+            _firstFrameTime = null;
+            _writer = new VideoFileWriter();
+            _writer.Open(dialog.FileName, (int)Math.Round(Image.Width, 0), (int)Math.Round(Image.Height, 0));
+            _recording = true;
+        }
+
+
+        private void SaveSnapshot()
+        {
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "Snapshot1";
+            dialog.DefaultExt = ".png";
+            var dialogresult = dialog.ShowDialog();
+            if (dialogresult != true)
+            {
+                return;
+            }
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(Image));
+            using (var filestream = new FileStream(dialog.FileName, FileMode.Create))
+            {
+                encoder.Save(filestream);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.Stop();
+            }
+            _writer?.Dispose();
+        }
+    }
+}
